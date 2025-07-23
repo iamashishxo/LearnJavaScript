dbab829a924d44ab2d549226a1cea9efe03ea0f7
iamashishxo <ashishrajput1234567890@gmail.com>
2025-07-23 23:54:15 +0530

    23-07-2025

---
+++ first.js
@@ -0,0 +1,27 @@
+// java is a dynamically typed language
+
+// const age = 23;
+
+// //age = 29 + 1;
+// console.log(age);
+//  let sr = 'Ashish';
+// console.log(sr[0]);
+
+let prices = [250, 645, 300, 900, 50];
+let discount = 10;
+
+for (let i = 0; i < prices.length; i++) {
+  prices[i] = prices[i] - prices[i] / discount;
+}
+for (let val of prices) {
+  console.log(val);
+}
+
+let companiesName = ["Microsoft", "Tesla", "google", "uber", "IBM", "Netflix"];
+
+companiesName.shift();
+companiesName.splice(2, 1, "ola");
+
+companiesName.push("amazon");
+
+console.log(companiesName);
